cbdf1281f91c261aa6eeff3bae012ce1afb999cd
zykor <enrique.wx@gmail.com>
2013-06-22 22:07:13 -0500

    mensaje

---
+++ usuarios/forms.py
@@ -0,0 +1,34 @@
+# -*- coding: utf-8 *-*
+from django import forms
+from django.utils.translation import ugettext_lazy as _
+from django.contrib.auth.models import User
+
+
+
+class UserField(forms.EmailField):
+    def clean(self, value):
+        super(UserField, self).clean(value)
+        try:
+            User.objects.get(username=value)
+            raise forms.ValidationError("El nombre de usuario ya existe. Por favor elija otro.")
+        except User.DoesNotExist:
+            return value
+
+class SignupForm(forms.Form):
+    first_name = forms.CharField(max_length=30, label=_(u"Empresa"))
+    last_name = forms.CharField(max_length=30, label=_(u"Nombre completo"))
+    email = UserField(label=_(u"Email"))
+    password = forms.CharField(widget=forms.PasswordInput(), label=_(u"Contraseña"))
+    password2 = forms.CharField(widget=forms.PasswordInput(), label=_(u"Repita contraseña"))
+
+
+
+    def clean_password(self):
+        if self.data['password'] != self.data['password2']:
+            raise forms.ValidationError('Las contraseñas no son iguales')
+        return self.data['password']
+    
+    def clean(self,*args, **kwargs):
+        self.cleaned_data.get('email')
+        self.clean_password()
+        return super(SignupForm, self).clean(*args, **kwargs)
--- usuarios/views.py
+++ usuarios/views.py
@@ -1,9 +1,13 @@
-# -*- coding: utf-8 *-*
+#encoding:utf-8
 from django.shortcuts import render_to_response
-from django.template import RequestContext
-from django.http import HttpResponseRedirect, HttpResponse
+from django.http import HttpResponse, HttpResponseRedirect
+from django.template import RequestContext #se agrega para poder utilizar la ruta de los archivos estatic se debe poner en todas las funciones
+from django.shortcuts import redirect
+from django.contrib.auth import login, authenticate, logout
 from django.contrib.auth.decorators import login_required
-from django.contrib.auth import logout
+from django.shortcuts import get_object_or_404
+from django.contrib.auth.models import User
+from forms import SignupForm
 from django.contrib.auth.models import User
 
 
@@ -22,3 +26,46 @@ def  logeado(request):
 def cerrar(request):
 	logout(request)
 	return HttpResponseRedirect('/')
+
+
+def nuevoUserView(request):
+	if request.user.is_authenticated() and not request.user.is_staff:
+		return redirect('/')
+	else:
+		if request.method == 'POST':
+			formulario = SignupForm(request.POST)
+			if formulario.is_valid():
+				username = formulario.cleaned_data['username']
+				password = formulario.cleaned_data['password']
+				email = formulario.cleaned_data['email']
+				new_user = User.objects.create_user(username, email ,password)
+				new_user.is_active=True
+				new_user.is_staff=False
+				new_user.is_superuser=False
+				new_user.first_name=formulario.cleaned_data['first_name']
+				new_user.save()
+				return HttpResponseRedirect('/')
+		else:
+			mensaje ="Los datos no son validos"
+			formulario =SignupForm()
+		return render_to_response('usuarios/registro.html',{'formulario': formulario }, \
+				context_instance=RequestContext(request))
+
+
+def login(request):
+	if request.user.is_authenticated():
+		return redirect('/')
+	else:
+		username = request.POST['username']
+		password = request.POST['password']
+		user = authenticate(username=username, password=password)
+		if user is not None:
+			if user.is_active:
+				login(request, user)
+				return redirect('/')
+			else:
+				return render_to_response('usuarios/login.html', \
+					context_instance=RequestContext(request))
+		else:
+			return render_to_response('usuarios/login.html', \
+				context_instance=RequestContext(request))
